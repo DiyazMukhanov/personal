5caad27277754060a02abd3645cbc30e7704f677
DiyazMukhanov <79104408+DiyazMukhanov@users.noreply.github.com>
2024-01-15 07:35:29 +0400

    Update App.tsx

--- tooltip/App.tsx
+++ tooltip/App.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useLayoutEffect, useState} from 'react';
 import { Tooltip } from './Tooltip/index';
 
 export default function App() {
+  const [tooltipLeft, setTooltipLeft] = useState(null)
+  const [tooltipTop, setTooltipTop] = useState(null)
+
+  useLayoutEffect(() => {
+    const tooltipedElement = document.querySelectorAll('.withTooltip')
+    tooltipedElement.forEach(element => {
+      let rect = element.getBoundingClientRect()
+      setTooltipLeft(rect.left + 20)
+      setTooltipTop(rect.top - 20)
+    })
+  }, [])
+
   return (
     <div className='App'>
       <Tooltip>
         {(onMouseEnter, onMouseLeave, showTooltip) => {
           return (
             <>
-              <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-                style={{ position: 'relative' }}
-              >
-                {showTooltip && (
-                  <div style={{ position: 'absolute', bottom: '20px', backgroundColor: 'orange', color: 'white', zIndex: '1' }}>
+             {showTooltip && (
+                  <div style={{ position: 'absolute', top: tooltipTop, left: tooltipLeft, backgroundColor: 'orange', color: 'white', zIndex: '1' }}>
                     Я тултип
                   </div>
                 )}
+              <div
+                className='withTooltip'
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+              >
+               
                 Наведи на меня
               </div>
             </>
