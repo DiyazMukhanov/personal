4432b609833f4633496e0f26ae487591b2a37127
DiyazMukhanov <79104408+DiyazMukhanov@users.noreply.github.com>
2024-01-16 20:33:15 +0400

    Update index.tsx

--- tooltip/Tooltip/index.tsx
+++ tooltip/Tooltip/index.tsx
@@ -1,15 +1,43 @@
-import { useState } from 'react';
+import { useState, useLayoutEffect } from 'react';
+import { createPortal } from 'react-dom';
 
-export function Tooltip({ children }) {
+export function Tooltip({ children, title }) {
   const [showTooltip, setShowTooltip] = useState(false);
+  const [tooltipLeft, setTooltipLeft] = useState(null);
+  const [tooltipTop, setTooltipTop] = useState(null);
 
-  const onMouseEnter = () => {
-    setShowTooltip(true);
-  };
+  useLayoutEffect(() => {
+    const tooltipedElement = document.querySelectorAll('.withTooltip');
+    tooltipedElement.forEach(element => {
+      let rect = element.getBoundingClientRect();
+      setTooltipLeft(rect.left + 20);
+      setTooltipTop(rect.top - 20);
+    });
+  }, []);
 
-  const onMouseLeave = () => {
-    setShowTooltip(false);
-  };
-  
-  return <>{children(onMouseEnter, onMouseLeave, showTooltip)}</>;
+  return (
+    <>
+      {showTooltip &&
+        createPortal(
+          <div
+            style={{
+              position: 'absolute',
+              top: tooltipTop,
+              left: tooltipLeft,
+              backgroundColor: 'orange',
+              color: 'white',
+              zIndex: '1',
+            }}
+          >{title}</div>,
+          document.body
+        )}
+      <span
+        className='withTooltip'
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
+      >
+        {children}
+      </span>
+    </>
+  );
 }
