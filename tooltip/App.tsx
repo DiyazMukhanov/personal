887178f62c45fefdaa1aa9e90a05eaafde0e6d9f
DiyazMukhanov <79104408+DiyazMukhanov@users.noreply.github.com>
2024-01-18 13:35:26 +0400

    Update App.tsx

--- tooltip/App.tsx
+++ tooltip/App.tsx
@@ -1,26 +1,24 @@
-import React, { useLayoutEffect, useState} from 'react';
+import React from 'react';
 import { Tooltip } from './Tooltip/index';
 
 export default function App() {
-  const [tooltipLeft, setTooltipLeft] = useState(null)
-  const [tooltipTop, setTooltipTop] = useState(null)
-
-  useLayoutEffect(() => {
-    const tooltipedElement = document.querySelectorAll('.withTooltip')
-    tooltipedElement.forEach(element => {
-      let rect = element.getBoundingClientRect()
-      setTooltipLeft(rect.left + 20)
-      setTooltipTop(rect.top - 20)
-    })
-  }, [])
 
   return (
     <div className='App'>
       <Tooltip title='Я тултип'>
-              <div>
-                Наведи на меня
-              </div>
+       {(onMouseEnter: (event: React.MouseEvent) => void, onMouseLeave:() => void) => {
+          return (
+            <div
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            >
+            Hover on me
+            </div>
+            )
+       }}
       </Tooltip>
+              
+
     </div>
   );
 }
